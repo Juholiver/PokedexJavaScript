3c425d210f987f0a4d23e82176a73c854733b04f
Jose Mario <juholiver3@gmail.com>
2022-11-22 11:56:51 -0300

    manipulando multiplas requisições em paralelo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,10 +2,17 @@
 // cosumindo a api na front end
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())}// transformando a lista em um json
+
+
 pokeApi.getPokemons = (offset =0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return fetch(url)
+    return fetch(url) // Buscando lista no servidor  = url 
         .then((response) => response.json()) //quando a promise for resolvida Convertendo o body para json 
-        .then((jsonBody) => jsonBody.results) //quando a promise reponse for convertida e resolvida se transforma jsonBody que com o resultado da json se trangforma em pkemon list
-        .catch((error) => console.error(error))
+        .then((jsonBody) => jsonBody.results) //transforma json em uma lista
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))//transformando a lista pokemon e uma lista de promisse da nova requisição do url
+        .then((detailRequests) => Promise.all(detailRequests))// pegando e esperando que todas informaçoes do pokemons termine
+        .then((pokemonsDetails) => pokemonsDetails) // devolvendo uma lista de detalhes de pokemon
 }
+
